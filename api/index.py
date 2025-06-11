4238270432a1b9471167ebf83c79f9ea3abe95cc
Satvik Mishra <satvikmishra2192@gmail.com>
2025-06-11 18:04:11 +0530

    Added deployed frontend link

--- api/index.py
+++ api/index.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Allow all origins (you can restrict this in production)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://rank-riser.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
